0991889b2430f2e7dcd3dc3ad7cea3ccc403a6c2
Jean-Francois Theroux <jtheroux@lapresse.ca>
2013-01-31 11:44:51 -0500

    initial commit

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 maintainer       "La Presse"
-maintainer_email "YOUR_EMAIL"
+maintainer_email "jtheroux@lapresse.ca"
 license          "All rights reserved"
 description      "Installs/Configures graphite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
